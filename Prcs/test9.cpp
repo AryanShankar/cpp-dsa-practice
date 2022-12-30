f6cc50742e9349ee5693be735c9db7f5715a6a61
Aryan Shankar <aryanshankar325@gmail.com>
2022-12-30 23:21:19 +0530

    Practiced various functions in cpp

---
+++ Prcs/test9.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    char ch = 'b';
+    string s = "haanoki";
+    cout<<ch<<endl;
+    cout<<toupper(ch)<<endl;
+    cout<<max(a,b)<<endl<<min(a,b)<<endl<<__gcd(a,b);
+}

e3f4566e40250423f9ff540b375d0edd7e1aa643
Aryan Shankar <aryanshankar325@gmail.com>
2023-01-01 23:23:44 +0530

    practised some ques and concepts

---
+++ Prcs/test14.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <int> vec {10,20,30,50};
+    for( auto x : vec)
+    cout<<x<<" ";
+
+    cout<<endl;
+
+    for(int i = 0; i<vec.size(); i++)
+    {
+        cout<<vec[i];
+    }
+}

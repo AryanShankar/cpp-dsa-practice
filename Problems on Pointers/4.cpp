8226be004e1bf02a113a8130372744067c483574
Aryan Shankar <aryanshankar325@gmail.com>
2023-01-04 23:34:08 +0530

    solved few questions on pointers

---
+++ Problems on Pointers/4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int *ptr = &x;
+    cout << *ptr << " ";
+    x = x + 30;
+    cout << *ptr << " ";
+    cout << ptr << " ";
+    *ptr = *ptr + 40;
+    cout << x << " ";
+    return 0;
+}
---
+++ Problems on Pointers/5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int *p1;
+    cout << p1 << " ";
+    cout << *p1 << " ";
+    return 0;
+}

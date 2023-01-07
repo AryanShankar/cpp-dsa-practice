4c7764e3e769340b2feff424a2c1b7f2bb511c29
Aryan Shankar <aryanshankar325@gmail.com>
2023-01-07 23:29:46 +0530

    solved questions on loops

---
+++ Problems on Loops/4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    for(;i < 10;)
+    {
+        cout << "GfG";
+        cout << " ";
+        i++;
+    }
+}
---
+++ Problems on Loops/5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    for(;;)
+    {
+        cout << "GfG";
+        cout << " ";
+        i++;
+    }
+}
